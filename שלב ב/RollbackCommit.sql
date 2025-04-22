c68a69944f1cb3228527f6d5f5076b26ab7e9bc8
Coral Shmuelovitz <167465751+CoralSh286@users.noreply.github.com>
2025-04-22 16:33:11 +0300

    Update and rename RollbackCommit.sql.txt to RollbackCommit.sql

--- שלב ב/RollbackCommit.sql
+++ שלב ב/RollbackCommit.sql
@@ -4,5 +4,13 @@ SET shift_schedule = 'morning'
 WHERE n_id = 1;
 SELECT * FROM nurse WHERE n_id = 1;
 ROLLBACK;
+SELECT * FROM nurse WHERE n_id = 1;
 
+
+BEGIN;
+UPDATE nurse 
+SET shift_schedule = 'night' 
+WHERE n_id = 1;
+SELECT * FROM nurse WHERE n_id = 1;
+COMMIT;
 SELECT * FROM nurse WHERE n_id = 1;
