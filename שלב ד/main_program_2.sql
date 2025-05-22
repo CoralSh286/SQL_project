27d6fd60aab1ab7be2405d9241eed69c0b70c540
Coral Shmuelovitz <167465751+CoralSh286@users.noreply.github.com>
2025-05-22 14:50:22 +0300

    Update main_program_2.sql

--- שלב ד/main_program_2.sql
+++ שלב ד/main_program_2.sql
@@ -1,1 +1,10 @@
-{\rtf1}
+DO $$
+DECLARE
+    treatment_count INTEGER;
+BEGIN
+    treatment_count := get_doctor_treatments(520);
+    RAISE NOTICE 'num treatment: %', treatment_count;
+  RAISE NOTICE ' ';
+    
+    CALL update_nurse_shift_new('1234119289');
+END $$;
