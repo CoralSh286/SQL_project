4fa33d69af4736fb1a5829cda208a996dd5ff55c
Coral Shmuelovitz <167465751+CoralSh286@users.noreply.github.com>
2025-05-22 14:49:22 +0300

    Update main_program_1.sql

--- שלב ד/main_program_1.sql
+++ שלב ד/main_program_1.sql
@@ -1,1 +1,19 @@
-{\rtf1}
+DO $$
+DECLARE
+    vol_cursor REFCURSOR;
+    rec_type VARCHAR;
+    rec_name VARCHAR;
+    rec_date DATE;
+BEGIN
+    vol_cursor := get_volunteer_info('smees2@illinois.edu', 'date', 'DESC');
+    
+    LOOP
+        FETCH vol_cursor INTO rec_type, rec_name, rec_date;
+        EXIT WHEN NOT FOUND;
+        RAISE NOTICE '% - %: %', rec_type, rec_name, rec_date;
+    END LOOP;
+    CLOSE vol_cursor;
+
+  
+    CALL assign_volunteer_to_patient_new(3, 3);
+END $$;
